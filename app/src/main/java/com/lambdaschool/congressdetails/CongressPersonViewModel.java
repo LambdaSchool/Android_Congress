fb55cd638873ead67ca94d5a4be2afa2dc5047b1
Patrick Martin <patrickmartin88@gmail.com>
2018-11-08 15:58:14 -0800

    bug fixes

--- app/src/main/java/com/lambdaschool/congressdetails/CongressPersonViewModel.java
+++ app/src/main/java/com/lambdaschool/congressdetails/CongressPersonViewModel.java
@@ -2,26 +2,25 @@ package com.lambdaschool.congressdetails;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
 
-import com.lambdaschool.congressdataapiaccess.CongresspersonDetails;
 import com.lambdaschool.congressdataapiaccess.CongresspersonOverview;
 
 import java.util.ArrayList;
 
-public class CongressPersonViewModel {
+public class CongressPersonViewModel extends ViewModel {
 
-    private MutableLiveData<ArrayList<CongresspersonDetails>> congressPersonDetailsList;
-    private CongressPersonOverviewRepo repo;
+    private MutableLiveData<ArrayList<CongresspersonOverview>> congressPersonDetailsList;
+
 
     public LiveData<ArrayList<CongresspersonOverview>> getCongresspersonOverviewList(){
         if(congressPersonDetailsList == null) {
-            //create list
+            loadlist();
         }
-
         return congressPersonDetailsList;
     }
 
     private void loadlist(){
-        cong
+        congressPersonDetailsList = CongressPersonOverviewRepo.getOverviewList();
     }
 }
