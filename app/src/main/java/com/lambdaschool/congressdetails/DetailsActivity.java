514d49ad03449a25457258a5913bae8adad69507
Patrick Martin <patrickmartin88@gmail.com>
2018-11-08 16:21:05 -0800

    bug fixes... i think

---
+++ app/src/main/java/com/lambdaschool/congressdetails/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.lambdaschool.congressdetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    public static final
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
