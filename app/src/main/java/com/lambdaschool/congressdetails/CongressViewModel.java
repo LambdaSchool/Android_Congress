d75ae16a8d17eae5ea9dfb6b37f4936b5c5724b9
Jake Esveld <thejakeesveld@gmail.com>
2019-05-02 13:09:55 -0700

    Add CongressViewModel class

---
+++ app/src/main/java/com/lambdaschool/congressdetails/CongressViewModel.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.congressdetails;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+import com.lambdaschool.congressdataapiaccess.CongresspersonOverview;
+
+import java.util.ArrayList;
+
+public class CongressViewModel {
+    private MutableLiveData<ArrayList<CongresspersonOverview>> data;
+    private CongressRepository repo;
+
+    public LiveData<ArrayList<CongresspersonOverview>> getCongress(){
+        if(data == null){
+            loadData();
+        }
+        return data;
+    }
+
+    private void loadData(){
+        repo = new CongressRepository();
+        data = repo.getData();
+    }
+
+    public void updateData(CongresspersonOverview person){
+        data.setValue(repo.UpdateData(person));
+    }
+}
