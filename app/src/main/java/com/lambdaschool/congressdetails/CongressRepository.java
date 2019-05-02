08baeb7c5ada0591f0101106818246ee0b6bede0
Jake Esveld <thejakeesveld@gmail.com>
2019-05-02 13:00:27 -0700

    Add CongressRepository Class

--- app/src/main/java/com/lambdaschool/congressdetails/CongressRepository.java
+++ app/src/main/java/com/lambdaschool/congressdetails/CongressRepository.java
@@ -1,4 +1,27 @@
 package com.lambdaschool.congressdetails;
 
+import android.arch.lifecycle.MutableLiveData;
+
+import com.lambdaschool.congressdataapiaccess.CongresspersonOverview;
+
+import java.util.ArrayList;
+
 public class CongressRepository {
+    private ArrayList<CongresspersonOverview> data;
+
+    public CongressRepository() {
+        data = new ArrayList<>();
+    }
+
+    public ArrayList<CongresspersonOverview> UpdateData(CongresspersonOverview person){
+        data.add(person);
+        return data;
+    }
+
+    public MutableLiveData<ArrayList<CongresspersonOverview>> getData (){
+        MutableLiveData<ArrayList<CongresspersonOverview>> liveData = new MutableLiveData<>();
+        liveData.setValue(data);
+
+        return liveData;
+    }
 }
