63c387d04913b4ad355e4c27f181c4a0cac92003
VivekV95 <vivekv210@gmail.com>
2019-05-02 17:18:08 -0500

    Added repo class for details activity

--- app/src/main/java/com/lambdaschool/congressdetails/ItemDetailFragment.java
+++ app/src/main/java/com/lambdaschool/congressdetails/ItemDetailFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.lambdaschool.congressdetails.dummy.DummyContent;
 
 /**
  * A fragment representing a single Item detail screen.
